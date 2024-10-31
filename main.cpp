4b0f5438f0baf5568374ab92298abe84117d2a92
1z2xh <114872750+1z2xh@users.noreply.github.com>
2024-10-31 19:54:06 +0800

    共享栈

--- main.cpp
+++ main.cpp
@@ -1,22 +1,98 @@
-#include <iostream>
-#include <thread>
-#include <vector>
-#include <functional> // for std::ref
+#include<stdio.h>
+#define SharedStackMax 100
+typedef char SharedStackType;
+typedef struct SharedStack{
+    SharedStackType data[SharedStackMax];
+    size_t top1;
+    size_t top2;
+}SharedStack;
 
-void increment(int& x) {
-    x++;
+void SharedStackInit(SharedStack*stack)
+{
+    if(stack==NULL)
+    {
+        return;
+    }
+    stack->top1=0;
+    stack->top2=SharedStackMax;
 }
-
-int main() {
-    int num = 0;
-
-    // 创建一个线程，使用 std::ref 传递 num 的引用
-    std::thread th(increment, std::ref(num));
-
-    // 等待线程完成
-    th.join();
-
-    std::cout << "Final value: " << num << std::endl; // 输出: Final value: 1
-
+void SharedStackPush1(SharedStack*stack,SharedStackType value)
+{
+    if(stack==NULL)
+    {
+        return;
+    }
+    if(stack->top1==stack->top2)
+    {
+        return;
+    }
+    stack->data[stack->top1++]=value;
+    return;
+}
+void SharedStackPush2(SharedStack*stack,SharedStackType value)
+{
+    if(stack==NULL)
+    {
+        return;
+    }
+    if(stack->top2==stack->top1)
+    {
+        return;
+    }
+    stack->data[--stack->top2]=value;
+}
+int  SharedStackTop1(SharedStack*stack,SharedStackType*value)
+{
+    if(stack==NULL||value==NULL)
+    {
+        return 0;
+    }
+    if(stack->top1==0)
+    {
+    // 返回0表示栈为空
     return 0;
+    }
+
+    // 从栈中弹出一个值
+    *value = stack->data[stack->top1-1];
+    // 成功弹出值后返回1
+    return 1;
+
+}
+int SharedStackTop2(SharedStack*stack,SharedStackType*value)
+{
+    if(stack==NULL||value==NULL)
+    {
+        return 0;
+    }
+    if(stack->top2==SharedStackMax)
+    {
+        return 0;
+    }
+    *value=stack->data[stack->top2];
+    return 1;
+}
+void SharedStackPop1(SharedStack*stack)
+{
+    if(stack==NULL)
+    {
+        return;
+    }
+    if(stack->top1==0)
+    {
+        return;
+    }
+    stack->top1--;
+}
+void SharedStackPop2(SharedStack*stack)
+{
+    if(stack==NULL)
+    {
+        return;
+    }
+    if(stack->top2==SharedStackMax)
+    {
+        return;
+    }
+    stack->top2++;
 }
