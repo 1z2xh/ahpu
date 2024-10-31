063b13b6d16796223283d5a9ef72b3e794f515cb
1z2xh <114872750+1z2xh@users.noreply.github.com>
2024-10-31 21:30:48 +0800

    async

--- main.cpp
+++ main.cpp
@@ -2,21 +2,12 @@
 // C++ Standard: C++17
 #include <iostream>
 #include <thread>
-// #include <mutex> //这个例子不需要mutex了
-#include <atomic>
+#include <future>
 using namespace std;
-atomic_int n = 0;
-void count10000() {
-    for (int i = 1; i <= 10000; i++) {
-        n++;
-    }
-}
 int main() {
-    thread th[100];
-    for (thread &x : th)//为每个线程分配任务 count10000，每个线程都会影响n的值
-        x = thread(count10000);
-    for (thread &x : th)
-        x.join();
-    cout << n << endl;
+    async(launch::async, [](const char *message){
+        cout << message << flush;
+    }, "Hello, ");
+    cout << "World!" << endl;
     return 0;
 }
